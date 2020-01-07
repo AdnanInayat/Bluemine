cd96dd201f8a15075c68ceb457a0ec87ce936350
asifahmad1011 <asif.apag@gmail.com>
2020-01-07 23:39:22 +0100

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
