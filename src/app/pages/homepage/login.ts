7cde5ea1bd2370568ad62a613c3fba89056a63a6
asifahmad1011 <asif.apag@gmail.com>
2020-01-09 02:17:52 +0100

    login page with navigation to register and dashboard

---
+++ src/app/pages/homepage/login.ts
@@ -0,0 +1,11 @@
+export interface Login {
+    email: string;
+    name: string;
+    gender: string;
+    phone: string;
+    address: string;
+    city: string;
+    country: string;
+    
+  }
+  
