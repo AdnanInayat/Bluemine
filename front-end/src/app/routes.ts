3957917b867ee5a32f6dc073a74aa57aeec07a0b
ameerhu <ameerhumzacheema@gmail.com>
2020-01-17 16:44:21 +0100

    1st update of ticket View page

--- front-end/src/app/routes.ts
+++ front-end/src/app/routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { TicketpageComponent } from './pages/ticketpage/ticketpage.component';
 
 export const appRoutes: Routes = [
     { path : 'register', component : RegisterComponent },
     { path : 'dashboard', component : DashboardComponent },
     { path : 'home', component : HomepageComponent },
+    { path : 'ticket', component : TicketpageComponent},
     { path : '', redirectTo : '/home', pathMatch : 'full' }
 ];
